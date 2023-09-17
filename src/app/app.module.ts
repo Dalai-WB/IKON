195523d4dddb50be943d6edcfadcc5f9f4512f77
Erdenedalai Erdenebat <erdenedalai.er@novelsoft.mn>
2023-09-18 01:26:29 +0800

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DailyComponent } from './daily/daily.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DailyComponent
   ],
   imports: [
     BrowserModule,
